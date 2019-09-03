efd8ee6ec13729096a8ffcec5eeeb24b7118d616
Kozo Nishida <kozo.nishida@gmail.com>
2019-09-03 10:38:29 +0900

    Create wp2cyjs.py

---
+++ wp2cyjs.py
@@ -0,0 +1,11 @@
+import json
+import requests
+
+import pandas as pd
+
+__author__ = 'Kozo Nishida'
+__email__ = 'knishida@riken.jp'
+__version__ = '0.0.1'
+__license__ = 'MIT'
+
+API_BASE = 'https://webservice.wikipathways.org/'
